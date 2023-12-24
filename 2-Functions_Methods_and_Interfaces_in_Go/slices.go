975c95a4f5cf34c3e61d96dfb85817b65bc394e5
Gabriel Barbosa Soares <gabriel.soares@teya.com>
2023-12-24 16:21:22 +0000

    2 course

---
+++ 2-Functions_Methods_and_Interfaces_in_Go/slices.go
@@ -0,0 +1,52 @@
+package __Functions_Methods_and_Interfaces_in_Go
+
+import "fmt"
+
+func main() {
+	// Slices
+	// Slices are a key data type in Go, giving a more powerful interface to sequences than arrays.
+	// Unlike arrays, slices are typed only by the elements they contain (not the number of elements).
+	// To create an empty slice with non-zero length, use the builtin make.
+	// Here we make a slice of strings of length 3 (initially zero-valued).
+	s := []string{"a", "b", "c"}
+	fmt.Println("emp:", s)
+
+	// We can set and get just like with arrays.
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	// len returns the length of the slice as expected.
+	fmt.Println("len:", len(s))
+
+	// In addition to these basic operations, slices support several more that make them richer than arrays.
+	// One is the builtin append, which returns a slice containing one or more new values.
+	// Note that we need to accept a return value from append as we may get a new slice value.
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	// Slices can also be copy’d.
+	// Here we create an empty slice c of the same length as s and copy into c from s.
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	// Slices support a “slice” operator with the syntax slice[low:high].
+	// For example, this gets a slice of the elements s[2], s[3], and s[4].
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	// This slices up to (but excluding) s[5].
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	// And this slices up from (and including) s[2].
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	// We can declare and initialize a variable for slice in a single line as well.
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	// Slices can be composed
+}
